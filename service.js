ceb512497e2e5d31be9eb41576915b06fa60b61a
Anu Sakpibal <nuhundred@gmail.com>
2019-10-22 14:40:24 +0700

    add more function service.js

--- service.js
+++ service.js
@@ -2,7 +2,7 @@ const url = require('url')
 
 exports.sampleRequest = function (req, res) {
     const reqUrl = url.parse(req.url, true)
-    var name = 'World'
+    var name = 'nuSapb'
     if (reqUrl.query.name) {
         name = reqUrl.query.name
     }
@@ -14,4 +14,31 @@ exports.sampleRequest = function (req, res) {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
     res.end(JSON.stringify(response))
+}
+
+exports.testRequest = function (req, res) {
+    body = ''
+
+    req.on('data', function (chunk) {
+        body += chunk
+    })
+
+    req.on('end', function () {
+
+        postBody = JSON.parse(body)
+
+        var response = {
+            "text": "Post Request Value is  " + postBody.value
+        }
+
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify(response))
+    })
+}
+
+exports.invalidRequest = function (req, res) {
+    res.statusCode = 404
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Invalid Request')
 }
